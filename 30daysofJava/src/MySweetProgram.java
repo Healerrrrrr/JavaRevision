d8b820ae93369d5a999678305424433b47eccc1e
Healerrrrrr <folzyacer@gmail.com>
2020-10-09 18:02:10 +0100

    updated

--- 30daysofJava/src/MySweetProgram.java
+++ 30daysofJava/src/MySweetProgram.java
@@ -1,2 +1,29 @@
-public class MySweetProgram {
-}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+    public class MySweetProgram {
+
+        public static void main(String[] args) throws FileNotFoundException {
+
+            Scanner in = new Scanner(new File("Family.txt"));
+
+            List<String> family = new ArrayList<String>();
+
+            while(in.hasNextLine()){
+                family.add(in.nextLine());
+            }
+
+            for(int i =0; i<family.size(); i++){
+                System.out.println("name: " + family.get(i));
+            }
+
+            in.close();
+
+
+        }
+    }
+
